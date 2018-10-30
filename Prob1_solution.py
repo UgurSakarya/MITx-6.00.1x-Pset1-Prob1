8c205b3a8366a2201cc0a14303a7c9ea58c33cf5
UgurSakarya <ugursakarya91@gmail.com>
2018-10-30 21:12:32 +0300

    Update and rename Prob1_solution to Prob1_solution.py

--- Prob1_solution.py
+++ Prob1_solution.py
@@ -1,15 +1,15 @@
-# MITx-6.00.1x-Pset1-Prob1
+-- MITx-6.00.1x-Pset1-Prob1
 Solution for MITx: 6.00.1x Introduction to Computer Science and Programming Using Python  Problem Set 1 and Problem 1
 
 
-# Assume s is a string of lower case characters.
+-- Assume s is a string of lower case characters.
 
-# Write a program that counts up the number of vowels contained in the string s. 
-# Valid vowels are: 'a', 'e', 'i', 'o', and 'u'.
-# For example, if s = 'azcbobobegghakl', your program should print:
-# Number of vowels: 5
+-- Write a program that counts up the number of vowels contained in the string s. 
+-- Valid vowels are: 'a', 'e', 'i', 'o', and 'u'.
+-- For example, if s = 'azcbobobegghakl', your program should print:
+-- Number of vowels: 5
 
-# NOTE: Do not define your own VALUES!
+-- NOTE: Do not define your own VALUES!
 
 count = 0      # defining count as 0 because we need to put them in a box as a increasing number
 for i in s:
